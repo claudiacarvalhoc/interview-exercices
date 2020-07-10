47d29e24a803f0f9c747a9ac7a7e021b7e597a7c
claudiacarvalhoc <claudia.a.carvalho.c@gmail.com>
2020-07-10 22:44:50 +0100

    add primitive solutions

---
+++ primitives/check-if-a-number-is-a-palindrome/Palindrome.cs
@@ -0,0 +1,70 @@
+using System;
+
+namespace primitives
+{
+    public class Palindrome
+    {
+        /*
+            Time: O(N) - linear time
+            Space: N * 2 - constant time
+            N is the number of digits of int
+        */
+        public bool isPalindrome_Solution1(int value)
+        {
+            // A negative number is not a palindrome
+            if (value < 0) {
+                return false;
+            }
+
+            int original = value;
+            int reverse = 0;
+
+            // while the value is greater then 0
+            // we will have digits to process
+            while (value >= 0) {
+                // Get last digit of integer
+                int digit = value % 10;
+                // Sum the digit to reverse integer
+                reverse = (reverse * 10) + digit;
+                // Remove the last digit from value integer
+                value = value / 10;
+            }
+
+            // the number is a palindrome
+            // if the original integer is equal to the reverse integer
+            return original == reverse;
+        }
+
+        /*
+            Time: O(N/2) - half of linear time
+            Space: N * 2 - constant time
+            N is the number of digits of int
+        */
+        public bool isPalindrome_Solution2(int value)
+        {
+            if (value < 0) {
+                return false;
+            }
+
+            // using the base 10 logarithm to:
+            // - get the number of digits 
+            // - get the left mask to extract left digit
+            int log10 = Math.Log10(value);
+            int numberOfDigits = log10 + 1;
+            int leftMask = Math.Pow(10, log10);
+            int rightMask = 10;
+
+            for (int i = 1; i <= numberOfDigits / 2; ++i) {
+                int rightDigit = value % rightMask;
+                int leftDigit = value / leftMask;
+                leftMask = leftMask / 10;
+
+                if (leftDigit != rightDigit) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ primitives/check-if-a-number-is-a-palindrome/readme.md
@@ -0,0 +1,26 @@
+Check If A Number Is A Palindrome
+A palindrome is a sequence that reads the same forwards and backward.
+
+Given an integer as input, write a function that tests if it is a palindrome.
+
+Example 1:
+Input: 9232
+Output: false
+
+Example 2:
+Input: 12321
+Output: true
+
+Example 3:
+Input: 1
+Output: true
+
+Example 4:
+Input: -121
+Output: false
+
+Note
+A negative number cannot be a palindrome due to the - sign
+
+Constraints
+You may not cast the number to a string
