0ae4d01430f6eb61f799113aea5c7aa5f233510a
claudiacarvalhoc <claudia.a.carvalho.c@gmail.com>
2020-07-11 01:15:39 +0100

    fb exercises - reverse operations

--- primitives/Program.cs
+++ primitives/Program.cs
@@ -6,6 +6,13 @@ namespace primitives
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Math.Sqrt(17));
+            Console.WriteLine(Math.Sqrt(17) % 2);
+            Console.WriteLine(Math.Sqrt(64));
+            Console.WriteLine(Math.Sqrt(64) % 2);
+            Console.WriteLine(Math.Pow(2, 10));
+            Console.WriteLine(Math.Pow(2, 6));
+            // Console.WriteLine(Math.Sqrt());
             Console.WriteLine("Hello World!");
         }
     }
